b6bcb1b7e7e0dbe57af4cb57c0bb76fae65854c9
richardcnc <richard.caneca@gmail.com>
2017-09-30 15:49:32 +0000

    subindo o projeto

---
+++ DemoCore/DemoCore/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoCore.Controllers
+{
+    public class HomeController:Controller
+    {
+        //[Route("batata/teste")] // exemplo de rota
+        //public string Index()
+        //{
+        //    return "Bateu no Home/Index";
+
+        //}
+
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+
+
+    }
+}
